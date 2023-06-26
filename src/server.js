21dd17272be26c2006aebcf47b432ba94eebbd86
Vasu <vasujain275@gmail.com>
2023-06-26 19:16:28 +0530

    Added Dynamic Addition

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path')
+const PORT = process.env.PORT || 3000
 
 const app = express();
 app.use(bodyParser.json());
@@ -100,6 +101,6 @@ app.use((req, res, next) => {
   res.status(404).send();
 });
 
-app.listen(3000,()=>{
-  console.log(`App Listening to Port 3000`)
+app.listen(PORT,()=>{
+  console.log(`App Listening to Port ${PORT}`);
 })
